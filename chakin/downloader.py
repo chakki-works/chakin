b5f9ae67e8ff719057d056cbcde987667de857aa
Hironsan <light.tree.1.13@gmail.com>
2017-05-23 14:39:08 +0900

    update downloader
    save_file's name don't change

--- chakin/downloader.py
+++ chakin/downloader.py
@@ -11,7 +11,7 @@ def load_datasets(path=os.path.join(os.path.dirname(__file__), 'datasets.csv')):
     return datasets
 
 
-def download(name='en-fastText', save_path='./words.vec'):
+def download(name, save_dir='./'):
     df = load_datasets()
     row = df[df.Name==name]
     url = ''.join(row.URL)
@@ -28,6 +28,8 @@ def download(name='en-fastText', save_path='./words.vec'):
 
         pbar.update(min(count * blockSize, totalSize))
 
+    file_name = url.split('/')[-1]
+    save_path = os.path.join(save_dir, file_name)
     urllib.request.urlretrieve(url, save_path, reporthook=dlProgress)
     pbar.finish()
 
@@ -35,7 +37,7 @@ def download(name='en-fastText', save_path='./words.vec'):
 def search(lang=''):
     df = load_datasets()
     if lang == '':
-        print(df[['Name', 'Lang', 'Method']])
+        print(df[['Name', 'Dimension', 'Corpus', 'VocabularySize', 'Method', 'Language', 'Author']])
     else:
-        rows = df[df.Lang==lang]
-        print(rows[['Name', 'Lang', 'Method']].head(n=30))
+        rows = df[df.Language==lang]
+        print(rows[['Name', 'Dimension', 'Corpus', 'VocabularySize', 'Method', 'Language', 'Author']])
